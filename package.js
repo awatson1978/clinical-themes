cb162d7126ba459ed96cc306c24c37a4ee1cb27a
Abigail Watson <awatson1978@gmail.com>
2015-10-05 21:53:01 -0700

    updated pacakges.js

--- package.js
+++ package.js
@@ -27,6 +27,6 @@ Package.onUse(function (api) {
 
 Package.onTest(function (api) {
   api.use('tinytest');
-  api.use('clinical-themes');
+  api.use('clinical:themes');
   api.addFiles('tests/clinical-themes.js');
 });
